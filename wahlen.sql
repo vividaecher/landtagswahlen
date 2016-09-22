b08ed1a34fe8de246c8e2a47b01ec4d75b0622a4
kucharski <kucharski@sipgate.de>
2016-09-22 11:20:18 +0200

    create clean nrw_2017 table

--- wahlen.sql
+++ wahlen.sql
@@ -1,58 +1,7 @@
--- MySQL dump 10.16  Distrib 10.1.16-MariaDB, for osx10.6 (i386)
---
--- Host: localhost    Database: wahlen
--- ------------------------------------------------------
--- Server version	10.1.16-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `nrw_2017`
---
-
 DROP TABLE IF EXISTS `nrw_2017`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `nrw_2017` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `linke` int(1) NOT NULL,
-  `spd` int(1) NOT NULL,
-  `gruenen` int(1) NOT NULL,
-  `fdp` int(1) NOT NULL,
-  `cdu` int(1) NOT NULL,
-  `afd` int(1) NOT NULL,
-  `hash` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `hash` (`hash`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nrw_2017`
---
-
-LOCK TABLES `nrw_2017` WRITE;
-/*!40000 ALTER TABLE `nrw_2017` DISABLE KEYS */;
-INSERT INTO `nrw_2017` VALUES (6,0,0,1,0,0,0,''),(37,0,0,1,0,0,0,'4571ba6924'),(43,0,0,0,1,0,0,'405de625ad'),(46,0,0,0,0,0,1,'03c5aa9d45'),(49,0,0,1,0,0,0,'c9d1d3a8d0');
-/*!40000 ALTER TABLE `nrw_2017` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-09-21 17:07:28
+  `party` varchar(10) NOT NULL,
+  `votes` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
