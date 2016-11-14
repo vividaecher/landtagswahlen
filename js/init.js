4affd300cda395401a9dd99fde883efdea17570b
Virginia Dächer <daecher@sipgate.de>
2016-11-14 08:12:34 +0100

    updated html structure

---
+++ js/init.js
@@ -0,0 +1,13 @@
+	$( ".navbar-toggle" ).click(function() {
+		$( this ).addClass( "-colapsed" );
+		$( ".hamburger-nav" ).addClass( "-folded-out" );
+	});
+
+/*$( document ).ready(function() {
+
+	$( ".navbar-toggle" ).click(function() {
+		$( this ).addClass( "-colapsed" );
+		$( ".hamburger-nav" ).addClass( "-folded-out" );
+	});
+
+};*/
