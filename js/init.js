6de85285c48981bf3d5644afda564b21e317c958
Virginia Dächer <daecher@sipgate.de>
2016-11-29 08:49:05 +0100

    adds close cross to mobile navigation

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
 
 $(".navbar-toggle").on("click", function() {
   $(".hamburger-nav").toggleClass("visible");
+  $(this).toggleClass("close-navigation");
   $("body").toggleClass("navigation-open");
 });
 
